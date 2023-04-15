3ed95632204a96c51fd2444d155ef201136f21b9
chinna reddy <chinnareddy2106@gmail.com>
2023-04-15 08:23:31 +0530

    execption update

---
+++ demo2.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from ML_Project.logger import logging
+from ML_Project.exception import CustomException
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try :
+        raise Exception("we are testing our exception file ")
+    except Exception as e:
+        ML_Project =CustomException(e,sys)
+        logging=info(visa.error_message )
+        logging-info("we are testing logging module")
+
+
+        return "helo world"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
